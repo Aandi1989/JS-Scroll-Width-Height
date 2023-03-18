39345c14b95c2ee9a1ace51a2b1c3deb4c9d3cb8
Aandi1989 <navaliny1989@gmail.com>
2023-03-18 13:32:49 +0300

    clientWidth and clientHeight

---
+++ script.js
@@ -0,0 +1,10 @@
+//Размер окна браузера
+// clientWidth и clientHeight
+
+// Доступная ширина и высота окна
+const mainElement = document.documentElement;
+const mainElementWidth = mainElement.clientWidth;
+const mainElementHeight = mainElement.clientHeight;
+
+console.log(mainElementWidth)
+console.log(mainElementHeight)
