5a97f2ab5bd544f6dc39ebcc23af8bb63a19ee1e
Aandi1989 <navaliny1989@gmail.com>
2023-03-19 09:12:43 +0300

    Управление скроллом (scrollIntoView)

--- script.js
+++ script.js
@@ -65,3 +65,34 @@ function setScrollToOptions(){
     });
 }
 //Опции не работают в Safari
+
+
+/*
+Вызов elem.scrollIntoView(top) прокручивает страницу,
+чтобы elem оказался вверху.У него есть один аргумент:
+
+- если top = true(по умолчанию), то страница будет прокручена,
+чтобы elem появился в верхней части окна.
+Верхний край элемента совмещён с верхней частью окна.
+- если top = false, то страница будет прокручена, чтобы elem
+появился внизу.Нижний край элемента будет совмещён с нижним краем окна.
+*/
+
+function setScrollIntoView(top) {
+	const lessonSelected = document.querySelector('.lesson__selected');
+	lessonSelected.scrollIntoView(top);
+}
+
+function setScrollIntoViewOptions(top) {
+	const lessonSelected = document.querySelector('.lesson__selected');
+	lessonSelected.scrollIntoView({
+		//"start", "center", "end" или "nearest". По умолчанию "center".
+		block: "center",
+		//"start", "center", "end" или "nearest". По умолчанию "nearest".
+		inline: "nearest",
+		// "auto" или "smooth". По умолчанию "auto".
+		behavior: "smooth"
+	});
+}
+//Опции не работают в Safari
+
